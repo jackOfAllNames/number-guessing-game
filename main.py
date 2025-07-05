ac1779bd19cb591e9ff6ba91f76764b2cecd8c6b
Olaitan Adedokun <kenzdex@gmail.com>
2025-07-05 15:00:42 +0100

    Handle both value and type errors

--- main.py
+++ main.py
@@ -3,28 +3,33 @@ from user_guess_selection import difficulty_selection, displayGameMenu
 
 def main():
 
-    displayGameMenu()
-    chances = difficulty_selection()
-    tries = chances
-    guess = random.randint(1, 5)
+    try:
+        displayGameMenu()
+        chances = difficulty_selection()
+        tries = chances
+        guess = random.randint(1, 5)
 
-    while chances > 0:
-        print(guess)
-        user_guess = int(input("Enter your guess: "))
-        
-        if user_guess > guess:
-            chances -= 1
-            print(f"Incorrect! The number is less than {user_guess}.")
-        elif user_guess < guess:
-            chances -= 1
-            print(f"Incorrect! The number is greater than {user_guess}.")
-        else:
-            chances -= 1
-            print(f"Congratulations! You guessed the correct number in {tries - chances} attempt(s).")
-            return
+        while chances > 0:
+            print(guess)
+            user_guess = int(input("Enter your guess: "))
+            
+            if user_guess > guess:
+                chances -= 1
+                print(f"Incorrect! The number is less than {user_guess}.")
+            elif user_guess < guess:
+                chances -= 1
+                print(f"Incorrect! The number is greater than {user_guess}.")
+            else:
+                chances -= 1
+                print(f"Congratulations! You guessed the correct number in {tries - chances} attempt(s).")
+                return
 
-    if not chances:
-        print("Game over\nGoodbye!")
+        if not chances:
+            print("Game over\nGoodbye!")
+    except TypeError:
+        print("Please enter a number (1, 2, or 3).")
+    except ValueError:
+        print("Please enter a valid number.")
 
 if __name__ == "__main__":
     main()
--- user_guess_selection.py
+++ user_guess_selection.py
@@ -12,21 +12,18 @@ def difficulty_selection():
     difficulty_level = {1: "Easy", 2: "Medium", 3: "Hard"}
     chances = [10, 5, 3]
 
-    try:
-        user_difficulty = int(input("Enter your choice: "))
-        match user_difficulty:
-            case 1:
-                difficulty_level = difficulty_level[1]
-            case 2:
-                difficulty_level = difficulty_level[2]
-            case 3:
-                difficulty_level = difficulty_level[3]
-            case _:
-                raise
+    user_difficulty = int(input("Enter your choice: "))
+    match user_difficulty:
+        case 1:
+            difficulty_level = difficulty_level[1]
+        case 2:
+            difficulty_level = difficulty_level[2]
+        case 3:
+            difficulty_level = difficulty_level[3]
+        case _:
+            return
 
-        if isinstance(difficulty_level, str):
-            print(f"\nGreat! You have selected the {difficulty_level} difficulty level.\n"
-                f"Let's start the game!\n")
-            return chances[user_difficulty - 1]
-    except:
-        print("Please enter a number (1, 2, or 3).")
+    if isinstance(difficulty_level, str):
+        print(f"\nGreat! You have selected the {difficulty_level} difficulty level.\n"
+            f"Let's start the game!\n")
+        return chances[user_difficulty - 1]
